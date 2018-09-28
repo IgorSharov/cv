ba12b49f3e02a6c116822278fa0b823d08828862
Igor Sharov <IgorSharov@users.noreply.github.com>
2018-09-28 18:18:05 +0300

    browsersync+watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,35 @@
-const { src, dest } = require('gulp');
+const {
+  src,
+  dest,
+  watch,
+  series,
+} = require('gulp');
+const browserSync = require('browser-sync').create();
 const slim = require('gulp-slim');
+const pump = require('pump');
+
+const siteDestFolder = './public';
+const slimSelector = './src/*.slim';
+
+function slimTransform() {
+  return pump([
+    src(slimSelector),
+    slim({ pretty: true }),
+    dest(siteDestFolder),
+  ]);
+}
 
 function development() {
-  return src('./src/*.slim')
-    .pipe(slim({ pretty: true }))
-    .pipe(dest('./public/'));
+  browserSync.init({
+    server: siteDestFolder,
+  });
+
+  watch(slimSelector,
+    { ignoreInitial: false },
+    series(
+      slimTransform,
+      browserSync.reload,
+    ));
 }
 
 exports.default = development;
